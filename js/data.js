d544d68a570cc08fa760d9f315ae5c1ea2a3ec18
Kost9in <Kost9inbi4@mail.ru>
2017-03-09 23:43:03 +0200

    Add test data.

---
+++ js/data.js
@@ -0,0 +1,12 @@
+const testData = [
+  { id: 1, parent: 0, title: 'item 1', description: 'desc 1' },
+  { id: 2, parent: 0, title: 'item 2', description: 'desc 2' },
+  { id: 3, parent: 0, title: 'item 3', description: 'desc 3' },
+  { id: 4, parent: 0, title: 'item 4', description: 'desc 4' },
+  { id: 5, parent: 1, title: 'child 1 item 1', description: 'desc 5' },
+  { id: 6, parent: 2, title: 'child 1 item 2', description: 'desc 6' },
+  { id: 7, parent: 2, title: 'child 2 item 2', description: 'desc 7' },
+  { id: 8, parent: 2, title: 'child 3 item 2', description: 'desc 8' },
+  { id: 9, parent: 2, title: 'child 4 item 2', description: 'desc 9' },
+  { id: 10, parent: 4, title: 'child 1 item 4', description: 'desc 10' }
+];
